8556f1d32bf08774aca528b1568314b20a33bc0c
Blackiceberg <theblackiceberg@gmail.com>
2022-02-05 16:37:51 +0100

    regex
    construction des regex  et des messages d'erreur

--- public/js/modal.js
+++ public/js/modal.js
@@ -36,10 +36,22 @@ const email = document.querySelector('#email');
 const birthdate = document.querySelector('#birthdate');
 const quantity = document.querySelector('#quantity');
 const conditionU = document.querySelector('#conditionU');
-//const locations= document.forms["form]['location'];
+const locations = document.querySelectorAll('[name="location"]:checked');
 
-// DOM Elements formulaire : location
-const locations = document.querySelector('#locations');
+//Regex
+const regexNomPrenom = /^[A-Za-z-]{2,}$/;
+const regexEmail = /\S+@\S+\.\S+/;
+const regexQuantity = /^0?([1-9][0-9]){1,}$/;
+
+
+// Messages d'erreurs
+const ErreurPrenom = "Le champ Prénom a un minimum de 2 caractères";
+const ErreurNom = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+const ErreurEmail = "L'adresse électronique n'est pas valide.";
+const ErreurBirthdate = "Vous devez entrer votre date de naissance.";
+const ErreurQuantity ="Vous devez entrer votre nombre de participation.";
+const ErreurLocations ="Vous devez choisir une option.";
+const ErreurConditionU = "Vous devez vérifier que vous acceptez les termes et conditions.";
 
 
 form.addEventListener('submit', e => {
@@ -65,53 +77,64 @@ const setSuccess = element => {
 };
 
 
-const isValidEmail = email => {
-  const regex = /\S+@\S+\.\S+/
-  return regex.test(String(email).toLowerCase());
+const isValidPrenom = prenom =>{
+  return regexNomPrenom.test(String(prenom).toLowerCase());
 }
+const isValidNom = nom =>{
+  return regexNomPrenom.test(String(nom).toLowerCase());
+}
+const isValidEmail = email => {
+  return regexEmail.test(String(email).toLowerCase());
+}
+
+const isValidQuantity = quantity => {
+  return regexQuantity.test(Number(quantity).toLowerCase());
+}
+
+
 
 const validateInputs = () => {
   const prenomValue = prenom.value.trim();
   const nomValue = nom.value.trim();
   const emailValue = email.value.trim();
   const birthdateValue = birthdate.value.trim();
- // const locationsValue = locations.value.trim();
+  //const locationsValue = location.checked;
   const conditionUValue = conditionU.checked;
 
-  /*if (location.checked==false){
-    setErreur(locations, "Vous devez choisir une option.")
+  /*if (locationsValue==false){
+    setErreur(locations,ErreurLocations)
   }else{
     setSuccess(locations);    
   }*/
 
-  if(!prenomValue){
-    setErreur(prenom, "Le champ Prénom a un minimum de 2 caractères")
+  if(!isValidPrenom(prenomValue)){
+    setErreur(prenom, ErreurPrenom)
   }else{
     setSuccess(prenom);
   }
-  if(!nomValue){
-    setErreur(nom, "Veuillez entrer 2 caractères ou plus pour le champ du nom."    )
+  if(!isValidNom(nomValue)){
+    setErreur(nom, ErreurNom)
   }else{
     setSuccess(nom);
   }
-
-  if(emailValue ===''){
-    setErreur(email, "renseigner votre adresse");
-  }else if (!isValidEmail(emailValue)){
-    setErreur(email,"L'adresse électronique n'est pas valide.")
+  if(!isValidEmail(emailValue)){
+    setErreur(email, ErreurEmail)
   }else{
     setSuccess(email);
   }
-
   if (!birthdateValue){
-    setErreur(birthdate, "Vous devez entrer votre date de naissance.")
+    setErreur(birthdate, ErreurBirthdate)
   }else{
     setSuccess(birthdate);
+  }
+  if (!isValidQuantity(quantity)){
+    setErreur(quantity, ErreurQuantity)
+  }else{
+    setSuccess(quantity);
      
   }
-
   if (conditionUValue == false){
-    setErreur(conditionU, "Vous devez vérifier que vous acceptez les termes et conditions.")
+    setErreur(conditionU, ErreurConditionU)
   }else{
     setSuccess(conditionU);
      
