fd108f0bdcbe67ae6ccb170511ea8e2e389a8033
Blackiceberg <theblackiceberg@gmail.com>
2022-02-15 20:20:39 +0100

    esc

--- public/js/modal.js
+++ public/js/modal.js
@@ -12,8 +12,6 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelectorAll(".close");
-
-
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
@@ -21,13 +19,10 @@ modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 // close modal event
 function closeModal() {
   modalbg.style.display = "none";
 }
-
-
 // DOM Elements formulaire
 const form = document.querySelector('#form');
 const prenom = document.querySelector('#prenom');
@@ -37,15 +32,10 @@ const birthdate = document.querySelector('#birthdate');
 const quantity = document.querySelector('#quantity');
 const conditionU = document.querySelector('#conditionU');
 const locations = document.querySelector("#locations");
-
-
 //Regex
 const regexNomPrenom = /^[a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]{2,}$/;
 const regexEmail = /\S+@\S+\.\S+/;
 const regexQuantity = /^[1-9]?[0-9]{1,1}$/;
-
-
-
 // Messages d'erreurs
 const ErreurPrenom = "Le champ Prénom a un minimum de 2 caractères";
 const ErreurNom = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
@@ -54,12 +44,10 @@ const ErreurBirthdate = "Vous devez entrer votre date de naissance.";
 const ErreurQuantity ="Vous devez entrer votre nombre de participation.";
 const ErreurLocations ="Vous devez choisir une ville.";
 const ErreurConditionU = "Vous devez vérifier que vous acceptez les termes et conditions.";
-
 // écouteur btn submit lance la vérification des champs
 form.addEventListener('submit', e => {
   e.preventDefault();
   validateInputs();
-
 }
 );
 // Fonction Messages d'erreurs 
@@ -67,7 +55,6 @@ form.addEventListener('submit', e => {
 const setErreur = (element, message) => {
   const inputControl = element.parentElement;
   const erreurDisplay = inputControl.querySelector('.erreur');
-
 // Fonction messages d'erreurs 
 //(ajoute ou supprime .success, .erreur)
   erreurDisplay.innerText = message;
@@ -83,7 +70,6 @@ const setSuccess = element => {
   inputControl.classList.add('success');
   inputControl.classList.remove('erreur');
 };
-
 // combinaison des constantInput et des regex 
 //(vérifications des contenue des input)
 const isValidPrenom = prenom =>{
@@ -108,7 +94,6 @@ const validateInputs = () => {
   const birthdateValue = birthdate.value.trim();
   const conditionUValue = conditionU.checked;
   const quantityValue = quantity.value.trim();
-
   if(!isValidPrenom (prenomValue)){
     setErreur(prenom, ErreurPrenom)
   }else{
@@ -134,14 +119,12 @@ const validateInputs = () => {
   }else{
     setSuccess(quantity);
   }
-
   if (conditionUValue == false){
     setErreur(conditionU, ErreurConditionU)
   }else{
     setSuccess(conditionU); 
   }
- // vérifie si au moins 1 boutton radio est séléctioné.
-
+  // vérifie si au moins 1 boutton radio est séléctioné.
   if (document.querySelectorAll('[name="location"]:checked').length < 1) {
   setErreur(locations, ErreurLocations)
   }else{
@@ -150,13 +133,11 @@ const validateInputs = () => {
   // vérifie si 7 de mes class success sont actives.
   if
   (document.querySelectorAll('.success').length >= 7){
-
   // fermé la modal, la relancé et y afficher un nouveau message.
     closeModal();
     launchModal();
     form.innerText = "Merci pour votre inscription";
     form.classList.add('register');
   }
-
 }
 
